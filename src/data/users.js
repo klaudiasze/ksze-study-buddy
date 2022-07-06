313004a0c62de334d5f5fc513ef7372d1c41e22e
Klaudia <klaudiaszenderowicz@gmail.com>
2022-07-06 13:33:10 +0200

    edition1

---
+++ src/data/users.js
@@ -0,0 +1,38 @@
+export const users = [
+  {
+    name: 'Adam Romański',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Krzysztof Batko',
+    attendance: '23%',
+    average: '3.3',
+  },
+  {
+    name: 'Patrycja Gonciarz',
+    attendance: '45%',
+    average: '4.3',
+  },
+  {
+    name: 'Olga Hahn',
+    attendance: '38%',
+    average: '2.3',
+  },
+  {
+    name: 'Paweł Andrzejewski',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Szymon Knuth',
+    attendance: '38%',
+    average: '2.3',
+  },
+
+  {
+    name: 'Krystian Foczpaniak',
+    attendance: '38%',
+    average: '2.3',
+  },
+];
