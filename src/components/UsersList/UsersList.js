fa1a4df12dc03c3c9087c01b11cf80dd49d9d62f
Klaudia <klaudiaszenderowicz@gmail.com>
2022-07-06 11:14:00 +0200

    git commit origin

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { users } from 'data/users';
-import UsersListItem from 'components/UsersListItem/UsersListItem';
+import UsersListItem from 'components/usersListItem/UsersListItem';
 
 const UsersList = () => (
   <div>
